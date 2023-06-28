904f762426f0daeb6c0955c599bb493cbf275a3e
NataBoud <natalia.boudard@gmail.com>
2023-06-28 11:42:05 +0200

    changement de couleurs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import './index.scss';
+import './index.scss'
+// import React from 'react'
+import { useState } from 'react'
 
 function App() {
+  // let count = 0
+  const [count, setCount] = useState(0)
+  // const [count, setCount] = React.useState(0)
+
+  const onClickPlus = () => {
+    // count++
+    setCount(count + 1)
+  }
+
+  const onClickMoins = () => {  
+    setCount(count - 1)
+  }
+
   return (
     <div className="App">
       <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h2>Comptoir</h2>
+        <h1>{count}</h1>
+        <button onClick={onClickMoins} className="minus">- Moins</button>
+        <button onClick={onClickPlus} className="plus">Plus +</button>
       </div>
     </div>
   );
 }
 
 export default App;
+
+
+// состояния в react // return - простая разметка jsx
+// пока обычный js
+//let count = 0 count++ console.log(count) - понимает только js для react нужно исп спкц hook useState
+
+//const onClickPlus = () => {count++} - прикручиваем на кнопку
+// код для React:// [count - var, setCount - f кот меняет var] 
